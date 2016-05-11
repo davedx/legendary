aa8b57d18533dddaa784caad337880f38ba25ef8
Dave Clayton <davedx@gmail.com>
2016-05-11 21:07:31 +0200

    Done refactoring input

--- app/src/Input.js
+++ app/src/Input.js
@@ -3,14 +3,11 @@ import events from './events';
 import Component from './Component';
 import $ from './constants';
 
-//refactor this shit
 class Input extends Component {
   constructor(parent, props = {}) {
-    super(parent);
+    super(parent, props);
 
-    this.props = props;
     this.handler = '';
-
     this.keyMap = {};
 
     events.on('setactiveinputhandler', (options) => {
@@ -21,58 +18,21 @@ class Input extends Component {
       if (!this.keyMap[event.keyCode]) {
         this.keyMap[event.keyCode] = true;
         const evName = this.handler != '' ? `${this.handler}:onkeydown` : 'onkeydown';
-        //console.info("emitting ", evName);
         events.emit(evName, {key: event.keyCode});
-      }
-
-      switch (event.keyCode) {
-        case 32: // space
-          if (this.travelMode === $.TravelModes.Normal) {
-            //if (this.canJump === true) this.velocity.y += 350;
-            this.canJump = false;
-          } else {
-            if (this.shiftDown) {
-              this.fly = -1;
-            } else {
-              this.fly = 1;
-            }
-          }
-          break;
-
-        case 16:
-          this.shiftDown = true;
-          break;
-
-        //default: console.info("unhandled key: "+event.keyCode);
       }
     };
 
     let onKeyUp = (event) => {
       if (this.keyMap[event.keyCode]) {
         this.keyMap[event.keyCode] = false;
-        events.emit('onkeyup', {key: event.keyCode});
-      }
-
-      switch(event.keyCode) {
-        case 32: // space
-          this.fly = 0;
-          break;
-
-        case 16:
-          this.shiftDown = false;
-          break;
+        const evName = this.handler != '' ? `${this.handler}:onkeyup` : 'onkeyup';
+        events.emit(evName, {key: event.keyCode});
       }
     };
 
     document.addEventListener('keydown', onKeyDown, false);
     document.addEventListener('keyup', onKeyUp, false);
-
-    this.canJump = false;
   }
-
-  // getObject() {
-  //   return this.controls.getObject();
-  // }
 }
 
 export default Input;
